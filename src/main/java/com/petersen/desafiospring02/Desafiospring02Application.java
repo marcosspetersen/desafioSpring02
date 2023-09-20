dcee0c3780ef22f72b793f9b17327eef14750b39
marcosspetersen <marcos_petersen@hotmail.com>
2023-09-20 16:09:06 -0300

    Start do projeto e criacao do back

---
+++ src/main/java/com/petersen/desafiospring02/Desafiospring02Application.java
@@ -0,0 +1,13 @@
+package com.petersen.desafiospring02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiospring02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafiospring02Application.class, args);
+	}
+
+}
